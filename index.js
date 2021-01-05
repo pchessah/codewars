784b499d42f8e2f89974506e84b61b800215032f
Paul Chesa <paulchesa1@gmail.com>
2021-01-05 08:21:49 +0300

    done

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-//Digital root is the recursive sum of all the digits in a number.
-//Given n, take the sum of the digits of n. If that value has more than one digit,
-//continue reducing in this way until a single-digit number is produced. 
-//The input will be a non-negative integer.
+//Given a non-negative integer, 3 for example, return a string with a murmur:
+//"1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 
-function digital_root(n) {
-  const addNumbers = (num) => {
-    let arr = num.toString().split("")
-    let sum = arr.map(item=>+item).reduce((a,b)=>a+b,0)
-    let arrSum = sum.toString().split("")
-    return arrSum
+
+var countSheep = function (num){
+  const str = ` sheep...`;
+  let ans = 1+str
+  for(let i = 2; i<num+1; i++){
+    ans = `${ans}${i}${str}`
   }
-  let ansArr = addNumbers(n);
-  if(ansArr.length === 1){
-    let ansStr = ansArr.join("")
-    let ans = +ansStr
-    console.log(ans);
-    return (ans);
-  } else {
-    let valStr = ansArr.join("")
-    let val = +valStr
-   return digital_root(val)
-  }
-  
+
+  return ans
+  //your code here
 }
-
-digital_root(456)
