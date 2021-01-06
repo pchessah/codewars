92f1264d53aa16d1a6abbf55e704b886000b26ec
Paul Chesa <paulchesa1@gmail.com>
2021-01-06 10:13:44 +0300

    done

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 //Given a non-negative integer, 3 for example, return a string with a murmur:
 //"1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 
-
-var countSheep = function (num){
-  const str = ` sheep...`;
-  let ans = 1+str
-  for(let i = 2; i<num+1; i++){
-    ans = `${ans}${i}${str}`
+// @ts-ignore
+String.prototype.toJadenCase = function () {
+  let arr = this.split(" ")
+  for(let i=0; i<arr.length; i++){
+    
   }
 
-  return ans
-  //your code here
-}
+  let ans= arr.map(item => item[0].toUpperCase()+item.substr(1)).join(" ");
+  console.log(ans);
+      
+  //...
+};
+
+var str = "How can mirrors be real if our eyes aren't real";
+// @ts-ignore
+str.toJadenCase()
