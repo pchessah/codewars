1d90d6d691d77b6abf61dbfb73643b5a5048f6bf
Paul Chesa <paulchesa1@gmail.com>
2021-01-08 16:01:50 +0300

    Done

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-function sumArray(array) {
-  if(array == null){
-    return 0
-  }
-  if(array.length<2){
-    return 0
+function check(a, x) {
+  if(a.includes(x)){
+    return true
   } else {
-    let highestValue = Math.max(...array)
-    let lowestValue = Math.min(...array)
-    let arrayWithoutHighAndLow = array.filter(item =>{
-      return item !== highestValue && item !== lowestValue
-    });
-    let ans = arrayWithoutHighAndLow.reduce((a,b)=>a+b,0)
-    return ans;
+    return false
   }
 }
 
-
-sumArray([ 6, 2, 1, 8, 10 ])
-
-//still not working
+//CHECK IF ITEM IS IN ARRAY
